5a4bf7549778ada5972e7e0cafb6ca55e8a1b7c6
Toni Arco <toniarcofrias@gmail.com>
2016-10-23 21:17:33 +0200

    Calculator working

--- app/Controller/HelloController.php
+++ app/Controller/HelloController.php
@@ -18,11 +18,27 @@ class HelloController extends AppController {
 
     function calculadora()
     {
-        if (empty($this->request->data))
-        {
+        $request = $this->request->data;
 
-        } else {
-            echo "calculado";
+        if (!empty($request))
+        {
+            $result = array();
+            $param1 = $request['#1'];
+            $param2 = $request['#2'];
+
+            // Cheking that booth params are numbers and not empty
+            if ( is_numeric($param1) && is_numeric($param2) )
+            {
+                $result = $param1 + $param2;
+                $response = array('code'=>'ok',
+                                  'msg' => 'El resultado de la operación es: '.$result);
+
+            } else {
+                $response = array('code'=>'ko',
+                                  'msg' => '**Comprueba que los campos sean númericos y esten completados');
+            }
+
+            $this->set('response',$response);
         }
 
     }
--- app/View/Hello/calculadora.ctp
+++ app/View/Hello/calculadora.ctp
@@ -1,7 +1,9 @@
+¡Ha llegado el momento de sumar!
 <?php echo $this->Form->create(false, array(
           'url' => array('controller' => 'hello', 'action' => 'calculadora')
       ));
 ?>
-<?php echo $this->Form->input('Primer Valor'); ?>
-<?php echo $this->Form->input('Segundo Valor'); ?>
+<?php echo $this->Form->input('#1'); ?>
+<?php echo $this->Form->input('#2'); ?>
 <?php echo $this->Form->end('Calculate'); ?>
+<?php if(isset($response))echo $response['msg'];?>
